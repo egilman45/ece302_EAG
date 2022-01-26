978ceac3826df8a0819671e965adcd16ced4c062
egilman45 <55217606+egilman45@users.noreply.github.com>
2022-01-26 13:59:55 -0500

    Add files via upload

---
+++ Homework 2/bag_interface_starter_code/bag_interface_starter_code/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/elizabeth/Documents/Homework 2/bag_interface_starter_code/bag_interface_starter_code
+# Build directory: /home/elizabeth/Documents/Homework 2/bag_interface_starter_code/bag_interface_starter_code
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittest "bag-tests")
+set_tests_properties(unittest PROPERTIES  _BACKTRACE_TRIPLES "/home/elizabeth/Documents/Homework 2/bag_interface_starter_code/bag_interface_starter_code/CMakeLists.txt;16;add_test;/home/elizabeth/Documents/Homework 2/bag_interface_starter_code/bag_interface_starter_code/CMakeLists.txt;0;")
